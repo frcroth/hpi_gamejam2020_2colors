dd130e0f184ae6732b19e47dc1269f9af092abb7
Felix Roth <felix.roth@student.hpi.uni-potsdam.de>
2020-02-29 13:09:07 +0100

    Added sound, added beat. BeatGiver scene should be instanced in every scene that uses beats, it calls on_beat on all beatable nodes

---
+++ BeatGiver.gd
@@ -0,0 +1,27 @@
+extends Node2D
+
+var beating = true
+var time = 1.0
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	beating_loop()
+
+
+func start_beating():
+	beating = true
+	
+func stop_beating():
+	beating = false
+	
+func beating_loop():
+	while beating:
+		yield (get_tree().create_timer(time),"timeout")
+		$AudioStreamPlayer.play(0)
+		get_parent().get_tree().call_group("beatable","on_beat")
+		
+	
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
