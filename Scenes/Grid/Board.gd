9dd2333bd993438ef4dffba7e0078baa82d1edb7
Tobias Kantusch <tobiaskantusch@online.de>
2020-02-29 16:41:36 +0100

    Board is now in middle

--- Scenes/Grid/Board.gd
+++ Scenes/Grid/Board.gd
@@ -7,28 +7,26 @@ var player2
 # var a = 2
 # var b = "text"
 
-var width = 4
-var height = 4
-var playfield = [[1, 0, 1, 0],
-				 [0, 1, 0, 1],
-				 [1, 0, 1, 0],
-				 [0, 1, 0, 1]]
+var width = 11
+var height = 11
+var playfield = []
 
 onready var tile = preload("res://Scenes/Grid/Tile.tscn")
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	playfield = generate_random_playfield(height,width)
 	init_playfield()
 	init_players()
 
 
 func init_playfield():
+	var tilestart = (get_viewport_rect().size.x - width * Globals.tilesize) / 2
+	playfield = generate_random_playfield(height,width)
 	for y in range(height):
 		for x in range(width):
 			var new_tile = tile.instance()
 			new_tile.current_color = Globals.blue if playfield[y][x] == 0 else Globals.red
-			new_tile.position = Vector2(x * Globals.tilesize, y * Globals.tilesize)
+			new_tile.position = Vector2(tilestart + x * Globals.tilesize, tilestart + y * Globals.tilesize)
 			new_tile.update_color()
 			add_child(new_tile)
 
