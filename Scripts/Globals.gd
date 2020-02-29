a6e69409d9ef79b0c67c32b0f2aab3ed75f048b7
Felix Roth <felix.roth@student.hpi.uni-potsdam.de>
2020-02-29 12:02:54 +0100

    Added blue and red values

---
+++ Scripts/Globals.gd
@@ -0,0 +1,14 @@
+extends Node
+
+var blue = Color("910b07")
+var red = Color("075c91")
+
+var tilesize = 64
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
